4c4d3e088869522930da3eef651df5ee55356178
nielsdylan <niels_dylan@hotmail.com>
2026-04-12 23:34:50 -0500

    cambios

--- src/app/panel-control/view/galeria/Albumes/AlbumDetalle.tsx
+++ src/app/panel-control/view/galeria/Albumes/AlbumDetalle.tsx
@@ -1,5 +1,16 @@
 import PageBreadcrumb from "@/components/PageBreadcrumb"
-import { Col, Container, Row } from "react-bootstrap"
+import { Card, CardBody, CardTitle, Col, Container, Row } from "react-bootstrap"
+import { Link } from "react-router"
+import addImage from '@/app/assets/images/add-image.jpg'
+
+const CardWithStretchedLink = () => {
+    return (
+        <Card className="border-primary border border-dashed">
+            <img src={addImage} className="card-img-top img-fluid" alt="..." width={373} height={233} />
+        </Card>
+    )
+}
+
 
 const AlbumDetalle = () => {
   return (
@@ -8,10 +19,11 @@ const AlbumDetalle = () => {
         <PageBreadcrumb title="Gestion de fotos del album" subtitle="" />
 
         <Row className="justify-content-center">
-          <Col xxl={8}>
-            {/* <CardTable /> */}
+          <Col xxl={2}>
+            <CardWithStretchedLink />
           </Col>
         </Row>
+        
       </Container>
     </div>
   )
