f81167d150bb656446018e8e16fdb3fa4fc8c9a7
nielsdylan <niels_dylan@hotmail.com>
2026-08-06 21:35:41 -0500

    asdf

--- src/app/panel-control/view/Dashboard.tsx
+++ src/app/panel-control/view/Dashboard.tsx
@@ -1,6 +1,5 @@
 import PageBreadcrumb from '@/components/PageBreadcrumb'
-import { Card, CardBody, Col, Container, ProgressBar, Row } from 'react-bootstrap'
-import { TbArrowUp } from 'react-icons/tb'
+import { Col, Container, Row } from 'react-bootstrap'
 import ComponentCard from '@/components/cards/ComponentCard'
 import CustomApexChart from '@/components/CustomApexChart.tsx'
 import { getColor } from '@/helpers/color'
@@ -100,16 +99,7 @@ const Dashboard = () => {
             </Col>
 
             <Col md={4}>
-                <Card className={`border shadow-none border-dashed mb-0 border-default bg-default`}>
-                    <CardBody>
-                        <div className="mb-3 d-flex justify-content-between align-items-center">
-                            <h5 className="fs-xl mb-0">4320</h5>
-                            <span className='d-flex justify-content-center align-items-center'>12.75%&nbsp;<TbArrowUp className={`text-success`} /></span>
-                        </div>
-                        <p className="text-muted mb-2"><span>Total reports generated</span></p>
-                        <ProgressBar now={12.75} variant={'secondary'} className='progress-sm' />
-                    </CardBody>
-                </Card>
+
             </Col>
         </Row>
         <Row>
